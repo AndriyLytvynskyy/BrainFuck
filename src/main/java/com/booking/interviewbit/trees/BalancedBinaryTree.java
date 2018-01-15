195ab0f18bfecd2a5e8a3b0204ddedc0f0c697d2
Andriy <andriy.lytvynskyy@gmail.com>
2018-01-15 09:50:53 +0200

    balanced binary tree

---
+++ src/main/java/com/booking/interviewbit/trees/BalancedBinaryTree.java
@@ -0,0 +1,60 @@
+package com.booking.interviewbit.trees;
+
+
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ */
+ //for each node, the two subtrees differ in height by no more than one
+
+// The "height" of a node in a binary tree is the length of longest path from that node to a leaf.
+
+//                  A  (Height = 2)
+//             /              \
+//        (height =-1)        B (Height = 1) <-- Unbalanced because 1-(-1)=2 >1
+//                            \
+//                    C (Height = 0)
+
+
+/**
+ *        A (h=3)
+ *      /         \
+ *   B(h=1)      C (h=2)
+ *   /            /   \
+ * D (h=0)     E(h=0)  F (h=1)
+ *  /
+ *  G (h=0)
+ */
+
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+       val = x;
+       left=null;
+       right=null;
+      }
+  }
+public class BalancedBinaryTree {
+    public static int getHeight(TreeNode root) {
+        if (root == null) return 0; // Base case
+        return Math.max(getHeight(root.left),
+                getHeight(root.right)) + 1;
+    }
+
+    public static int isBalancedFunc(TreeNode root) {
+        if (root == null) return 1; // Base case
+        int heightDiff =
+                getHeight(root.left) -
+                        getHeight(root.right);
+        if (Math.abs(heightDiff) > 1) {
+            return 0;
+        } else { // Recurse
+            return isBalancedFunc(root.left) * isBalancedFunc(root.right);
+        }
+    }
+      public int isBalanced(TreeNode A) {
+           return isBalancedFunc(A);
+    }
+}
