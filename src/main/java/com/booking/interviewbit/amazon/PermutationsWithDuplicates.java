5fd17151f6f66e421dfb2658c369760d9cfdb427
Andriy <andriy.lytvynskyy@gmail.com>
2018-01-15 09:50:34 +0200

    Permutation with duplicates

--- src/main/java/com/booking/interviewbit/amazon/PermutationsWithDuplicates.java
+++ src/main/java/com/booking/interviewbit/amazon/PermutationsWithDuplicates.java
@@ -1,8 +1,6 @@
 package com.booking.interviewbit.amazon;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Basic idea is to permutate with Hash Tables
@@ -11,34 +9,46 @@ import java.util.Set;
  */
 public class PermutationsWithDuplicates {
 
-    Map<Character, Integer> buildFreqMap(String s) {
+    static Map<Character, Integer> buildFreqMap(String s) {
         Map<Character, Integer> freqMap = new HashMap<>();
         for (char ch : s.toCharArray()) {
             if (freqMap.containsKey(ch)) {
                 Integer value = freqMap.get(ch);
                 freqMap.put(ch, ++value);
             } else {
-                freqMap.put(ch, 0);
+                freqMap.put(ch, 1);
             }
         }
         return freqMap;
     }
 
 
-    public void printPerms(String s) {
+    public static void printPerms(String s) {
+        List<String> result = new ArrayList<>();
         Map<Character, Integer> map = buildFreqMap(s);
+        printPerms(map, "", s.length(), result);
+    }
+
+    public static void printPerms( Map<Character, Integer> freqMap, String prefix, int remaining, List<String> result) {
+        if (remaining ==0){
+            result.add(prefix);
+            return;
+        }
+        Set<Character> keySet = freqMap.keySet();
+        for (Character character : keySet) {
+            int count = freqMap.get(character);
+            if(count>0) {
+                freqMap.put(character, count - 1);
+                printPerms(freqMap, prefix + character, remaining - 1, result);
+                freqMap.put(character, count);
+            }
+        }
 
     }
 
-    public void printPerms(int remaining, String prefix, Map<Character, Integer> freqMap) {
-        Set<Character> keySet = freqMap.keySet();
-
-        for (Character character : keySet) {
-            int count = freqMap.get(character);
-            freqMap.put(character, count - 1);
-            printPerms(0, "", freqMap);
-            freqMap.put(character, count);
-        }
+    public static void main(String[] args) {
+        //printPerms("ABC");
+        printPerms("AB");
     }
 
 }
