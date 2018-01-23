773c3925323baba06718ac07e31b0e72db23a3b5
andrii_lytvynskyi <Andrii_Lytvynskyi@epam.com>
2018-01-23 11:23:33 +0200

    Some Tree problems

---
+++ src/main/java/com/booking/interviewbit/trees/BinaryTreeInvert.java
@@ -0,0 +1,38 @@
+package com.booking.interviewbit.trees;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, invert the binary tree and return it.
+ *  Look at the example for more details.
+ *
+ * Solution is explained here:
+ * https://leetcode.com/problems/invert-binary-tree/solution/
+ *
+ */
+public class BinaryTreeInvert {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+
+    }
+}
