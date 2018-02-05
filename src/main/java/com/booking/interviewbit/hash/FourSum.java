47b0f3182eec2c6b7e4d1edee6dc1be5e33e9221
Andriy <andriy.lytvynskyy@gmail.com>
2018-02-05 21:33:56 +0200

    Four Sum copied solution

--- src/main/java/com/booking/interviewbit/hash/FourSum.java
+++ src/main/java/com/booking/interviewbit/hash/FourSum.java
@@ -42,70 +42,26 @@ class Tuple {
 public class FourSum {
 
     public static ArrayList<ArrayList<Integer>> fourSum(ArrayList<Integer> A, int B) {
-        ArrayList<ArrayList<Integer>> set = new ArrayList<ArrayList<Integer>>();
+        Set<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
 
-//        Collections.sort(set, new Comparator<ArrayList<Integer>>() {
-//            @Override
-//            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-//                List<Integer> copy1 = new ArrayList<>(o1);
-//                List<Integer> copy2 = new ArrayList<>(o2);
-//
-//                Collections.sort(copy1);
-//                Collections.sort(copy2);
-//                Iterator<Integer> it1 = o1.iterator();
-//                Iterator<Integer> it2 = o2.iterator();
-//                while (it1.hasNext()) {
-//                    Integer t1 = it1.next();
-//                    Integer t2 = it2.next();
-//                    if (Integer.compare(t1, t2) != 0) {
-//                        // as soon as a difference is found, stop looping
-//                        return 0;
-//                    }
-//                }
-//
-//                return 1;
-//            }
-
-        //Set<ArrayList<Integer>> set = new HashSet<>();
-//        Set<ArrayList<Integer>> set = new TreeSet<>(new Comparator<ArrayList<Integer>>() {
-//            @Override
-//            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-//                List<Integer> copy1 = new ArrayList<>(o1);
-//                List<Integer> copy2 = new ArrayList<>(o2);
-//
-//                Collections.sort(copy1);
-//                Collections.sort(copy2);
-//                Iterator<Integer> it1 = o1.iterator();
-//                Iterator<Integer> it2 = o2.iterator();
-//                while (it1.hasNext()) {
-//                    Integer t1 = it1.next();
-//                    Integer t2 = it2.next();
-//                    if (Integer.compare(t1, t2) != 0) {
-//                        // as soon as a difference is found, stop looping
-//                        return 0;
-//                    }
-//                }
-//
-//            return 1;
-//            }
-//        }
-//
-//        );
         ArrayList<Tuple> twoSum = twoSum(A, B);
         for (int index = 0; index < twoSum.size(); index++) {
             Tuple first = twoSum.get(index);
-            for (int j = index + 1; j< twoSum.size(); j++){
-                ArrayList<Integer> tempArray = new ArrayList<>();
-                tempArray.add(first.index1);
-                tempArray.add(first.index2);
-                Tuple tuple = twoSum.get(j);
-                tempArray.add(tuple.index1);
-                tempArray.add(tuple.index2);
-                Collections.sort(tempArray);
-                set.add(tempArray);
+            for (int j = index + 1; j < twoSum.size(); j++){
+                if (first != twoSum.get(j)) {
+                    ArrayList<Integer> tempArray = new ArrayList<>();
+                    tempArray.add(first.index1);
+                    tempArray.add(first.index2);
+                    Tuple tuple = twoSum.get(j);
+                    tempArray.add(tuple.index1);
+                    tempArray.add(tuple.index2);
+                    Collections.sort(tempArray);
+                    set.add(tempArray);
+                }
             }
         }
-        Collections.sort(set, new Comparator<ArrayList<Integer>>() {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>(set);
+        Collections.sort(result, new Comparator<ArrayList<Integer>>() {
             @Override
             public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
                 List<Integer> copy1 = new ArrayList<>(o1);
@@ -118,25 +74,55 @@ public class FourSum {
                 while (it1.hasNext()) {
                     Integer t1 = it1.next();
                     Integer t2 = it2.next();
-                    if (Integer.compare(t1, t2) != 0) {
-                        // as soon as a difference is found, stop looping
-                        return 1;
-                    }
+                    int i = Integer.compare(t1, t2);
+                    if (i !=0 ) return i;
                 }
-
                 return 0;
             }
-        });
 
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>(set);
+
+        });
+        return result;
+    }
+
+    public ArrayList<ArrayList<Integer>> fourSum_1(ArrayList<Integer> A, int B) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        Integer[] numbers = A.toArray(new Integer[A.size()]);
+
+        Arrays.sort(numbers);
+
+        for (int i = 0; i < numbers.length - 3; i++) {
+            for (int j = i + 1; j < numbers.length - 2; j++) {
+                int left = j + 1;
+                int right = numbers.length - 1;
+
+                while (left < right) {
+                    int sum = numbers[i] + numbers[j] + numbers[left] + numbers[right];
+                    if (sum == B) {
+                        ArrayList<Integer> tuple = new ArrayList<>();
+                        tuple.add(numbers[i]);
+                        tuple.add(numbers[j]);
+                        tuple.add(numbers[left]);
+                        tuple.add(numbers[right]);
+                        if (!result.contains(tuple)) {
+                            result.add(tuple);
+                        }
+                        left++;
+                        right--;
+                    } else if (sum < B) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
         return result;
     }
 
     public static ArrayList<Tuple> twoSum(final List<Integer> A, int B) {
         Set<Integer> set = new HashSet<Integer>(A);
-
-
-        List<Tuple> set2Sum = new ArrayList<Tuple>();
+        Set<Tuple> set2Sum = new HashSet<Tuple>();
         for (int index = 0; index < A.size(); index++) {
             if (set.contains(B - A.get(index))) {
                 int comb1 = Math.min(A.get(index), A.get(A.indexOf(B - A.get(index))));
@@ -150,12 +136,13 @@ public class FourSum {
 
     public static void main(String[] args) {
         //Integer[] array = {4, 7, -4, 2, 2, 2, 3, -5, -3, 9, -4, 9, -7, 7, -1, 9, 9, 4, 1, -4, -2, 3, -3, -5, 4, -7, 7, 9, -4, 4, -8 };
-        Integer[] array = {9, -8, -10, -7, 7, -8, 2, -7, 4, 7, 0, -3, -4, -5, -1, -4, 5, 8, 1, 9, -2, 5, 10, -5, -7, -1, -6, 4, 1, -5, 3, 8, -4, -10, -9, -3, 10, 0, 7, 9, -8, 10, -9, 7, 8, 0, 6, -6, -7, 6, -4, 2, 0, 10, 1, -2, 5, -2};
-        //Integer[] array = {1, 0, -1, 0, -2, 2};
+        //Integer[] array = {9, -8, -10, -7, 7, -8, 2, -7, 4, 7, 0, -3, -4, -5, -1, -4, 5, 8, 1, 9, -2, 5, 10, -5, -7, -1, -6, 4, 1, -5, 3, 8, -4, -10, -9, -3, 10, 0, 7, 9, -8, 10, -9, 7, 8, 0, 6, -6, -7, 6, -4, 2, 0, 10, 1, -2, 5, -2};
+        Integer[] array = {1, 0, -1, 0, -2, 2};
         final List<Integer> list = Arrays.asList(array);
-        ArrayList<Integer> ls = fourSum(new ArrayList(list), 0);
-        for (Integer l : ls) {
-            System.out.println(l);
-        }
+        ArrayList<ArrayList<Integer>> ls = fourSum(new ArrayList(list), 0);
+        System.out.println(ls);
+//        for (Integer l : ls) {
+//            System.out.println(l);
+//        }
     }
 }
