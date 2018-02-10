3b03370150f666280f19f2bb4ebb7c85f92de5a9
Andriy <andriy.lytvynskyy@gmail.com>
2018-02-10 21:09:34 +0200

    Minor refactoring Palindrom List

--- src/main/java/com/booking/interviewbit/linkedlist/PalindromList.java
+++ src/main/java/com/booking/interviewbit/linkedlist/PalindromList.java
@@ -1,16 +1,5 @@
 package com.booking.interviewbit.linkedlist;
 
-/**
- * Given a singly linked list, determine if its a palindrome.
- * Return 1 or 0 denoting if its a palindrome or not, respectively.
- */
-//Solution is borrowed from:
-// https://www.sigmainfy.com/blog/leetcode-palindrome-linked-list-o1-space.html
-class ListNode {
-      public int val;
-      public ListNode next;
-      ListNode(int x) { val = x; next = null; }
-}
 public class PalindromList {
     public void exchange2Numbers (int a, int b){
         int temp = a;
