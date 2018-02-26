ac451a7f09dd2b57049a19634f8c39162ba04ca0
andrii_lytvynskyi <Andrii_Lytvynskyi@epam.com>
2018-02-26 11:44:19 +0200

    WaysToFormMaxHeap Task

--- src/main/java/com/booking/interviewbit/heap/WaysToFormMaxHeap.java
+++ src/main/java/com/booking/interviewbit/heap/WaysToFormMaxHeap.java
@@ -26,16 +26,191 @@ import java.util.List;
  * https://www.geeksforgeeks.org/number-ways-form-heap-n-distinct-integers/
  *
  */
-public class WaysToFormMaxHeap {
-    public int solve(int A) {
-        return 0;
+
+class SolutionWorkingWaysToFormHeap {
+    public class Solution {
+
+        long[] dp;  	/* dp[i] = number of max heaps for i distinct integers */
+        long[][] nck;	/* nck[i][j] = i choose j if i>=j else 0 */
+        int[] log2;		/* log2[i] = int(log base 2 of i) */
+
+        final long MOD = 1000000007;
+
+        public long choose(int n,int k)
+        {
+            if(k>n)
+                return 0;
+            if(n<=1)
+                return 1;
+            if(k==0)
+                return 1;
+
+            if(nck[n][k]!=-1)
+                return nck[n][k];
+            long answer = choose(n-1,k-1) + choose(n-1,k);
+            answer%=MOD;
+            nck[n][k] = answer;
+            return answer;
+        }
+
+        public int getL(int n)
+        {
+            if(n==1)
+                return 0;
+
+            int h = log2[n];
+            int p = n - ((1<<(h)) - 1);
+            int m = (1<<h);
+            if(p>=(m/2))
+                return (1<<(h)) - 1;
+            else
+                return (1<<(h)) - 1 - ((m/2) - p);
+        }
+
+        public int solve(int n)
+        {
+            dp = new long[n+1];
+            for(int i=0;i<=n;i++)
+                dp[i]=-1;
+
+            nck = new long[n+1][n+1];
+            for(int i=0;i<=n;i++)
+                for(int j=0;j<=n;j++)
+                    nck[i][j] = -1;
+
+            log2 = new int[n+1];
+            int currLogAnswer = -1;
+            int currPower2 = 1;
+            for(int i=1;i<=n;i++)
+            {
+                if(currPower2==i)
+                {
+                    currLogAnswer++;
+                    currPower2*=2;
+                }
+                log2[i] = currLogAnswer;
+            }
+
+            return (int)getNumberOfMaxHeaps(n);
+        }
+
+        public long getNumberOfMaxHeaps(int n)
+        {
+            if(n<=1)
+                return 1;
+
+            if(dp[n]!=-1)
+                return dp[n];
+
+            int L = getL(n);
+            long ans = (choose(n-1,L)*getNumberOfMaxHeaps(L))%MOD*(getNumberOfMaxHeaps(n-1-L));
+            ans%=MOD;
+            dp[n] = ans;
+            return ans;
+        }
     }
+
+}
+public class WaysToFormMaxHeap {
+    private static final int MAXN = 1000;
+
+    // dp[i] = number of max heaps for i distinct integers
+    int dp[] = new int[MAXN];
+
+    // nck[i][j] = number of ways to choose j elements
+    //      form i elements, no order
+    int nck[][] = new int[MAXN][MAXN];
+
+    // log2[i] = floor of logarithm of base 2 of i
+    int log2[] = new int[MAXN];
+
+    // to calculate nCk
+    int choose(int n, int k)
+    {
+        if (k > n)
+            return 0;
+        if (n <= 1)
+            return 1;
+        if (k == 0)
+            return 1;
+
+        if (nck[n][k] != -1)
+            return nck[n][k];
+
+        int answer = choose(n - 1, k - 1) + choose(n - 1, k);
+        nck[n][k] = answer;
+        return answer;
+    }
+
+
+    int getLeft(int n)
+    {
+        if (n == 1)
+            return 0;
+
+        int h = log2[n];
+
+        // max number of elements that can be present in the
+        // hth level of any heap
+        int numh = (1 << h); //(2 ^ h)
+
+        // number of elements that are actually present in
+        // last level(hth level)
+        // (2^h - 1)
+        int last = n - ((1 << h) - 1);
+
+        // if more than half-filled
+        if (last >= (numh / 2))
+            return (1 << h) - 1; // (2^h) - 1
+        else
+            return (1 << h) - 1 - ((numh / 2) - last);
+    }
+
+    // find maximum number of heaps for n
+    int numberOfHeaps(int n) {
+
+        if (n <= 1)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int left = getLeft(n);
+        int ans = (choose(n - 1, left) * numberOfHeaps(left)) *
+                (numberOfHeaps(n - 1 - left));
+        dp[n] = ans;
+        return ans;
+    }
+
+    public long solve(int n) {
+        for (int i = 0; i <= n; i++)
+            dp[i] = -1;
+
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= n; j++)
+                nck[i][j] = -1;
+
+        int currLog2 = -1;
+        int currPower2 = 1;
+
+        // for each power of two find logarithm
+        for (int i = 1; i <= n; i++) {
+            if (currPower2 == i) {
+                currLog2++;
+                currPower2 *= 2;
+            }
+            log2[i] = currLog2;
+        }
+
+        return numberOfHeaps(n);
+    }
+
+
 
     public static void main(String[] args) {
        List<Integer> list =  Arrays.asList(1,2,3,4,5);
-       for ( int i = list.size()/2; i > 0; i -- ){
-
-       }
+       WaysToFormMaxHeap ways = new WaysToFormMaxHeap();
+        System.out.println(ways.solve(20) % 1000000007);
     }
 
 
